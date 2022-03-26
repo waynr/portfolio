71917f68b267faa28f52d90cb8d821f3d08b4991
Wayne Warren <wayne.warren.s@gmail.com>
2022-03-25 23:27:28 -0600

    main: implement stubbed out distribution endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use axum::{
+    extract::Path,
+    routing::{delete, get, post, patch},
+    Router,
+};
+
+
+#[tokio::main]
+async fn main() {
+    let blobs = Router::new().route( "/:digest", get(notimplemented).delete(notimplemented).head(notimplemented))
+        .route("/uploads/", post(notimplemented))
+        .route("/uploads/:reference", patch(notimplemented).put(notimplemented));
+    let manifests = Router::new().route("/:reference", delete(notimplemented).put(notimplemented).get(notimplemented).head(notimplemented));
+
+    let tags = Router::new().route("/list", get(notimplemented));
+
+    let app = Router::new().route("/:registry/v2", get(hello_world))
+        .nest("/:registry/v2/:repository/blobs", blobs)
+        .nest("/:registry/v2/:repository/manifests", manifests)
+        .nest("/:registry/v2/:repository/tags", tags);
+
+    axum::Server::bind(&"0.0.0.0:13030".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+
+async fn notimplemented(Path(params): Path<HashMap<String, String>>) -> String { 
+    format!("not implemented\n{:?}", params)
+}
+
+async fn hello_world() -> String {
+    "hello world".to_string()
 }
