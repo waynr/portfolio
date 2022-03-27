7659a1cca73bab539580aef7b9312a2ae8dee34a
Wayne Warren <wayne.warren.s@gmail.com>
2022-03-26 22:35:48 -0600

    http: simplify routes by remove registry name

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,10 @@ async fn serve() {
 
     let tags = Router::new().route("/list", get(notimplemented));
 
-    let app = Router::new().route("/:registry/v2", get(hello_world))
-        .nest("/:registry/v2/:repository/blobs", blobs)
-        .nest("/:registry/v2/:repository/manifests", manifests)
-        .nest("/:registry/v2/:repository/tags", tags);
+    let app = Router::new().route("/v2", get(hello_world))
+        .nest("/v2/:repository/blobs", blobs)
+        .nest("/v2/:repository/manifests", manifests)
+        .nest("/v2/:repository/tags", tags);
 
     axum::Server::bind(&"0.0.0.0:13030".parse().unwrap())
         .serve(app.into_make_service())
