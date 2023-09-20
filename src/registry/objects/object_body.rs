0b0e7219cd231942dddb07847c2557855135acb2
wayne warren <wayne.warren.s@gmail.com>
2023-09-20 12:48:24 -0600

    registry: clean impl Stream for StreamObjectBody

--- src/registry/objects/object_body.rs
+++ src/registry/objects/object_body.rs
@@ -38,16 +38,6 @@ impl StreamObjectBody {
         // https://doc.rust-lang.org/std/pin/#pinning-is-not-structural-for-field
         unsafe { &mut self.get_unchecked_mut().body }
     }
-
-    // fn pin_get_body(self: Pin<&mut Self>) -> Pin<&mut ObjectBody> {
-    //     // this is "safe" because body is never considered pin, see
-    //     // https://doc.rust-lang.org/std/pin/#pinning-is-structural-for-field
-    //     unsafe { self.map_unchecked_mut(|s| &mut s.body) }
-    // }
-
-    // fn pin_get_digester(&mut self) -> &mut T {
-    //     unsafe { &mut self.get_unchecked_mut().digester }
-    // }
 }
 
 impl Stream for StreamObjectBody {
@@ -57,21 +47,13 @@ impl Stream for StreamObjectBody {
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let object_body = self.pin_get_body();
         match Pin::new(&mut object_body.body).poll_next(cx) {
-            Poll::Ready(opt) => {
-                match opt {
-                    Some(result) => {
-                        match result {
-                            Ok(bytes) => {
-                                // and this is where we calculate incremental digest
-                                object_body.digester.update(bytes.as_ref());
-                                Poll::Ready(Some(Ok(bytes)))
-                            }
-                            Err(e) => Poll::Ready(Some(Err(Box::new(e)))),
-                        }
-                    }
-                    None => Poll::Ready(None),
-                }
+            Poll::Ready(Some(Ok(bytes))) => {
+                // and this is where we calculate incremental digest
+                object_body.digester.update(bytes.as_ref());
+                Poll::Ready(Some(Ok(bytes)))
             }
+            Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(Box::new(e)))),
+            Poll::Ready(None) => Poll::Ready(None),
             Poll::Pending => Poll::Pending,
         }
     }
