5a3276fd69910414300f6d48d5e06e128658aba8
wayne warren <wayne.warren.s@gmail.com>
2023-09-23 19:49:48 -0600

    s3/logging: impl read_after_deserialization

--- src/registry/objects/s3/logging.rs
+++ src/registry/objects/s3/logging.rs
@@ -13,7 +13,18 @@ impl aws_sdk_s3::config::Interceptor for LoggingInterceptor {
         _cfg: &mut aws_sdk_s3::config::ConfigBag,
     ) -> Result<(), aws_sdk_s3::error::BoxError> {
         let request = context.request();
-        println!("request = {:?}", request);
+        println!("read_after_serialization: request = {request:?}");
+        Ok(())
+    }
+
+    fn read_after_deserialization(
+        &self,
+        context: &aws_sdk_s3::config::interceptors::AfterDeserializationInterceptorContextRef<'_>,
+        _runtime_components: &aws_sdk_s3::config::RuntimeComponents,
+        _cfg: &mut aws_sdk_s3::config::ConfigBag,
+    ) -> Result<(), aws_sdk_s3::error::BoxError> {
+        let response = context.response();
+        println!("read_after_deserialization: response = {response:?}");
         Ok(())
     }
 }
