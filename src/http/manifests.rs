3f997f753d4027f2ed2795c04b778371a9d869e4
wayne warren <wayne.warren.s@gmail.com>
2023-09-27 22:04:57 -0600

    http/manifests: implement HEAD /v2/<name>/manifests/<reference>

--- src/http/manifests.rs
+++ src/http/manifests.rs
@@ -22,8 +22,51 @@ pub fn router<O: ObjectStore>() -> Router {
         get(get_manifest::<O>)
             .delete(notimplemented)
             .put(notimplemented)
-            .head(notimplemented),
+            .head(head_manifest::<O>),
     )
+}
+
+async fn head_manifest<O: ObjectStore>(
+    Extension(registry): Extension<Registry<O>>,
+    Path(path_params): Path<HashMap<String, String>>,
+) -> Result<Response> {
+    let repo_name = match path_params.get("repository") {
+        Some(s) => s,
+        None => return Err(Error::MissingPathParameter("repository")),
+    };
+
+    let manifest_ref = ManifestRef::from_str(
+        path_params
+            .get("reference")
+            .ok_or_else(|| Error::MissingQueryParameter("reference"))?,
+    )?;
+
+    let repository = match registry.get_repository(repo_name).await {
+        Err(e) => {
+            tracing::warn!("error retrieving repository: {e:?}");
+            return Err(Error::DistributionSpecError(
+                DistributionErrorCode::NameUnknown,
+            ));
+        }
+        Ok(r) => r,
+    };
+
+    let mstore = repository.get_manifest_store();
+    let manifest = mstore.get_manifest(&manifest_ref).await?;
+
+    if let Some(manifest) = manifest {
+        let mut headers = HeaderMap::new();
+        let dgst: String = manifest.digest.into();
+        headers.insert(
+            HeaderName::from_lowercase(b"docker-content-digest")?,
+            HeaderValue::from_str(dgst.as_str())?,
+        );
+        return Ok((StatusCode::OK, headers, "").into_response());
+    }
+
+    Err(Error::DistributionSpecError(
+        DistributionErrorCode::ManifestBlobUnknown,
+    ))
 }
 
 async fn get_manifest<O: ObjectStore>(
