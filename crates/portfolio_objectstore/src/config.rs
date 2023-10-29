43e35773941afde943856264a7e68ee287d7cca4
wayne warren <wayne.warren.s@gmail.com>
2023-10-29 15:25:48 -0600

    portfolio_objectstore: update Config to use serde "type" tagging

--- crates/portfolio_objectstore/src/config.rs
+++ crates/portfolio_objectstore/src/config.rs
@@ -6,14 +6,15 @@ use super::ObjectStore;
 use super::Result;
 
 #[derive(Clone, Deserialize)]
+#[serde(tag = "type")]
 pub enum Config {
-    S3Config(super::s3::S3Config),
+    S3(super::s3::S3Config),
 }
 
 impl Config {
     pub async fn new_objects(&self) -> Result<Arc<dyn ObjectStore>> {
         match self {
-            Self::S3Config(cfg) => Ok(Arc::new(cfg.new_objects().await?)),
+            Self::S3(cfg) => Ok(Arc::new(cfg.new_objects().await?)),
         }
     }
 }
